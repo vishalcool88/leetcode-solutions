808ec44211e92c3050b89c1d1ea1b2999a4b6779
vishal agrawal <vagrawal@processia.com>
2021-02-09 10:32:07 -0500

    Updated comment

--- 240. Search a 2D Matrix II/Solution.java
+++ 240. Search a 2D Matrix II/Solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         for (int i = 0; i < matrix.length; i++) {
+            //check - in for loop to break the loop if target is grater than the matrix[i][j]
             for (int j = 0; j < matrix[0].length && matrix[i][j]<=target; j++) {
                 if (matrix[i][j] == target) {
                     return true;

5acfce6c2a62558ad761f0556513d6c6fe4ddd4e
vishal agrawal <9411725+vishalcool88@users.noreply.github.com>
2021-02-22 13:02:52 -0500

    Create Solution.java

---
+++ 277. Find the Celebrity/Solution.java
@@ -0,0 +1,27 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    
+    private int numberOfPeople;
+    
+    public int findCelebrity(int n) {
+        numberOfPeople = n;
+        for (int i = 0; i < n; i++) {
+            if (isCelebrity(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    private boolean isCelebrity(int i) {
+        for(int j=0;j<numberOfPeople;j++){
+            if(i==j)
+                continue;
+            if(knows(i,j) || !knows(j,i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}

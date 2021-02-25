a41bba484afb152ceea5d0cbcaf0e625e49ae2e1
vishal agrawal <9411725+vishalcool88@users.noreply.github.com>
2021-02-24 22:12:42 -0500

    BFS solution using queue

---
+++ 101. Symmetric Tree/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean flag = true;
+        queue.add(root);
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if(node1==null && node2==null) continue;
+            if(node1==null || node2==null) return false;
+            if(node1.val==node2.val){
+                queue.add(node1.left);
+                queue.add(node2.right);
+                queue.add(node1.right);
+                queue.add(node2.left);
+            } else{
+                return false;
+            }
+        }
+        return flag;
+    }
+}
